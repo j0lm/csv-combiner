a64bcaea6a7a6c331296c7588a37b9f080258028
j0lm <jose0lmedojr@gmail.com>
2023-01-16 16:15:40 -0600

    wrote foundation, need to test

--- csv-combiner.py
+++ csv-combiner.py
@@ -1,3 +1,17 @@
-# Jose Olmedo Jr
-# csv-combiner
+import sys, os
 
+
+def main(file_paths):
+    print('combining csv files...')
+    for fp in file_paths:
+        # get file name
+        file_name = os.path.basename(fp)
+        file = open(fp)
+        # output each line and append filename
+        for line in file:
+            print(line + "," + file_name)
+        file.close()
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
